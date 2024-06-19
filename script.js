c99afb4d278365c511cb9ad2c0888d2fe471616c
MiguelFEWebDev <173039673+MiguelFEWebDev@users.noreply.github.com>
2024-06-19 10:57:50 +0200

    Initial implementation of the Modal

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-// TEST
-// const me = "Maikel";
-// console.log(me);
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal");
+const modal = document.querySelector("[data-modal]");
+const overlay = document.querySelector("[data-overlay]");
+
+openButton.addEventListener("click", () => {
+  modal.classList.add("open");
+  overlay.classList.add("open");
+});
+
+closeButton.addEventListener("click", () => {
+  modal.classList.remove("open");
+  modal.classList.remove("overlay");
+});
