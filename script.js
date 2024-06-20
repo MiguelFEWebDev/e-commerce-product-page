ef4d4d591aa8e73b367b634bd96e577f3ef00241
MiguelFEWebDev <173039673+MiguelFEWebDev@users.noreply.github.com>
2024-06-20 13:55:44 +0200

    Added counter functionality

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
+//Modal
+
 const openButton = document.querySelector("[data-open-modal]");
 const closeButton = document.querySelector("[data-close-modal");
+
 const modal = document.querySelector("[data-modal]");
 const overlay = document.querySelector("[data-overlay]");
 
-openButton.addEventListener("click", () => {
-  modal.classList.add("open");
-  overlay.classList.add("open");
-});
+// Counter
 
-closeButton.addEventListener("click", () => {
-  modal.classList.remove("open");
-  overlay.classList.remove("open");
-});
+let count = 0;
 
+let currentAmount = document.querySelector(".currentAmount");
+let addItem = document.querySelector(".b-add");
+let removeItem = document.querySelector(".b-remove");
+
+function itemsAdd() {
+  count++;
+  currentAmount.textContent = count;
+}
+
+function itemsRemove() {
+  if (count > 0) count--;
+  currentAmount.textContent = count;
+}
+
+addItem.addEventListener("click", itemsAdd);
+removeItem.addEventListener("click", itemsRemove);
+
+// adding to basket
+
+//Swiper
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "vertical",
@@ -23,4 +40,16 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+//Opening modal on click
+openButton.addEventListener("click", () => {
+  modal.classList.add("open");
+  overlay.classList.add("open");
+});
+
+//Closing modal on click
+closeButton.addEventListener("click", () => {
+  modal.classList.remove("open");
+  overlay.classList.remove("open");
 });
